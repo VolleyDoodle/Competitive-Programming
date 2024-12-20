e4c3ae61254f22ad4cffcc59669b342b2b45547e
VolleyDoodle <aarooshr@gmail.com>
2024-12-19 23:25:14 -0800

    moorbles finished :)

--- USACO/moorbles/moorbles.cpp
+++ USACO/moorbles/moorbles.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+//Even Even Even Even Even Even Even Even Even Even Even Even Odd Even Even Odd
+//Even Even Even Even Even Even Even Even Odd Even Even Even Even Even Even Even
 void solve(){
     int n, m, k;
     cin >> n >> m >> k;
@@ -27,9 +29,10 @@ void solve(){
         }
     }
     //now build the sequence with the greatest amount remaing from i..m
-    vector <long long> amtLeft(m + 1, 0);
+    vector <long long> amtLeft(m + 1, 0), pfsDip(m + 1, 0), curDip(m + 1);
     vector <int> curAns(m);
     for (int i = m - 1; i >= 0; i--){
+        //pfsDip[i] = pfsDip[i + 1];
         amtLeft[i] = amtLeft[i + 1];
         if (cards[i][0]%2 != cards[i][1]%2){
             amtLeft[i]-=min(cards[i][0], cards[i][1]);
@@ -38,47 +41,74 @@ void solve(){
             amtLeft[i]+=min(cards[i][0], cards[i][1]);
             curAns[i] = cards[i][0]%2;
         }
+        curDip[i] = amtLeft[i] - amtLeft[i + 1];
+        //pfsDip[i] = min(pfsDip[i], amtLeft[i] - amtLeft[i + 1]);
     }
-    if (n + amtLeft[0] <= 0){
-        cout << -1; return;
+    vector <long long> biggestDip(m + 1, 0);
+    for (int i = 1; i < m + 1; i++){
+        pfsDip[i] = pfsDip[i - 1];
+        pfsDip[i]+=curDip[i - 1];
+        
     }
-    long long diff = 0;
+    for (int i = m; i >= 1; i--){
+        if (i == m){
+            biggestDip[i] = pfsDip[i];
+            continue;
+        }
+        biggestDip[i] = min(pfsDip[i], biggestDip[i + 1]);
+    }
+    long long keepTrack = 0;
     for (int i = 0; i < m; i++){
         //nothing can change to sequence if below is true
-        if (curAns[i] == 0) continue;
+        if (n + (amtLeft[0] - amtLeft[i + 1]) <= 0){
+            cout << -1; return;
+        }
+        if (curAns[i] == 0){
+            if (cards[i][0]%2 != cards[i][1]%2){
+                keepTrack+=cards[i][1];
+            } else{
+                keepTrack-=min(cards[i][0],cards[i][1]);
+            }
+            continue;
+        }
         //so only when curAns == 1 is when we want to see if can switch
         //two cases
         //first case, one odd and one even, try to see if possible to take even
         if (cards[i][0]%2 != cards[i][1]%2){
-            diff+= cards[i][1] - cards[i][0];
-            if (n + (amtLeft[i] - diff) > 0){
+            //diff+= cards[i][1] - cards[i][0];
+            
+            keepTrack += cards[i][1];
+            if ((n - keepTrack) + amtLeft[i + 1] > 0 && n - keepTrack > 0 && (n - keepTrack) + biggestDip[i + 1] - pfsDip[i + 1] > 0){
                 curAns[i] = 0;
             } else{
-                diff-=cards[i][1] - cards[i][0];
+                keepTrack-=cards[i][1];
+                keepTrack+=cards[i][0];
             }
         } else{
-            diff+=max(cards[i][0], cards[i][1]) + min(cards[i][0], cards[i][1]);
-            if (n + (amtLeft[i] - diff) > 0){
+            keepTrack+= max(cards[i][0], cards[i][1]);
+            if ((n - keepTrack) + amtLeft[i + 1] > 0 && n - keepTrack > 0 && (n - keepTrack) + biggestDip[i + 1] - pfsDip[i + 1] > 0 ){
                 curAns[i] = 0;
             } else{
-                diff-=max(cards[i][0], cards[i][1]) + min(cards[i][0], cards[i][1]);
+                keepTrack-=max(cards[i][0], cards[i][1]);
+                keepTrack-=min(cards[i][0], cards[i][1]);
             }
         }
         
         //second case, two odds, and then try to tank the biggest odd
     }
-    for (int i: curAns){
-        if (i == 1){
+    for (int i = 0; i < curAns.size() - 1; i++){
+        if (curAns[i] == 1){
             cout << "Odd ";
         }
         else{
             cout << "Even ";
         }
     }
+    cout << (curAns[curAns.size() - 1] == 1 ? "Odd" : "Even");
 }
 int main(){
-    freopen("txt.in", "r", stdin);
-    freopen("txt.out", "w", stdout);
+    //freopen("txt.in", "r", stdin);
+    //freopen("txt.out", "w", stdout);
     int t;
     cin >> t;
     while (t--){
